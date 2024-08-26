f6b6af28dfc7c43ccf4485c138aed86730734b56
Hichamsss <serifihicham@hotmail.fr>
2024-08-26 21:48:26 +0200

    Ajout Environment Variables

--- src/main/java/be/iccbxl/pid/Config/StripeConfig.java
+++ src/main/java/be/iccbxl/pid/Config/StripeConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StripeConfig {
 	
-    private String apiKey = "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";
+    private String apiKey = "stripe.apiKey";
 
     public String getApiKey() {
         return apiKey;
