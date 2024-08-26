01c09b0732e1a6a956f6a642911d44b466e00da1
Hichamsss <serifihicham@hotmail.fr>
2024-08-26 15:21:38 +0200

    Correction mail Service

--- src/main/java/be/iccbxl/pid/Service/EmailService.java
+++ src/main/java/be/iccbxl/pid/Service/EmailService.java
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     @Autowired
-    private JavaMailSender javaMailSender;
+    private JavaMailSender emailSender;
 
     public void sendConfirmationEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
+        SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setFrom("serifi-100@hotmail.com"); // Explicitement définir l'expéditeur
+        message.setTo(to); 
+        message.setSubject(subject); 
         message.setText(text);
-        javaMailSender.send(message);
+        emailSender.send(message);
     }
 }
