6bc60a9fa9b04aa11efe47f0fb0a3a6b2e8d0b8c
Hichamsss <serifihicham@hotmail.fr>
2024-08-31 18:04:48 +0200

    Version finale

--- src/main/java/be/iccbxl/pid/Config/StripeConfig.java
+++ src/main/java/be/iccbxl/pid/Config/StripeConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StripeConfig {
 	
-    private String apiKey = "stripe.apiKey";
+    private String apiKey = "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";
 
     public String getApiKey() {
         return apiKey;
